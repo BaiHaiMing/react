80c9f1ffd6e559f8ee020cc256f48ae8fe687692
BaiHaiMing <56330816+BaiHaiMing@users.noreply.github.com>
2020-04-15 13:39:34 +0800

    day5

---
+++ 05/todolist/src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <img src={require("./logo.svg")} alt=""/>
+      </div>
+    )
+  }
+}
+
+export default App;
